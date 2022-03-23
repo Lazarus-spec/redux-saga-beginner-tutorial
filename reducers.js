a51503f11badbf43f7570b20968ac6546bd24415
lanziwen <lan_ziwen@163.com>
2022-03-23 23:36:19 +0800

    takeEvery获取store；saga的select函数

--- reducers.js
+++ reducers.js
@@ -1,3 +1,12 @@
-export default function counter(state , action) {
-    console.log('reducer')
+export default function counter(state={} , action) {
+    console.log('reducer',action)
+    switch (action.type) {
+        case 'takeEvery':
+            return {
+                ...state,
+                ...action.user
+            }
+        default:
+            return state
+    }
 }
--- sagas.js
+++ sagas.js
@@ -1,10 +1,13 @@
-import {takeEvery,takeLatest,throttle} from 'redux-saga/effects'
+import {takeEvery,takeLatest,throttle,select} from 'redux-saga/effects'
 
 // 负责启动其他的sagas函数
 export default function* rootSaga() {
     // 三个辅助函数都是监听action的，只要action触发就会被监听到
     yield takeEvery('takeEvery',function *() {
-        console.log('takeEvery')
+        // reducer 先执行，saga获取其值，之后处理生成新的store
+        // select 可以拿到当前 state
+        const state = yield select()
+        console.log('takeEvery',state)
     })
     //
     yield takeLatest('takeLatest',function *() {
