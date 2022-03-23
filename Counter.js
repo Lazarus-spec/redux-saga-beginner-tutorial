bb324ca254271b5c8ad1a7a771ecb0dde06e8864
lanziwen <lan_ziwen@163.com>
2022-03-23 21:53:38 +0800

    init

--- Counter.js
+++ Counter.js
@@ -1,20 +1,24 @@
 import React from 'react'
 import { PropTypes } from 'prop-types';
 
-const Counter = ({ value, onIncrement, onDecrement }) =>
-      <div>
+const Counter = ({ value, onIncrement, onDecrement, onIncrementAsync }) =>
+    <div>
+        <button onClick={onIncrementAsync}>
+            Increment after 1 second
+        </button>
+        {' '}
         <button onClick={onIncrement}>
-          Increment
+            Increment
         </button>
         {' '}
         <button onClick={onDecrement}>
-          Decrement
+            Decrement
         </button>
         <hr />
         <div>
-          Clicked: {value} times
+            Clicked: {value} times
         </div>
-      </div>
+    </div>
 
 Counter.propTypes = {
   value: PropTypes.number.isRequired,
--- main.js
+++ main.js
@@ -3,11 +3,16 @@ import "@babel/polyfill"
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { createStore, applyMiddleware } from 'redux'
+import createSagaMiddleware from 'redux-saga'
 
 import Counter from './Counter'
 import reducer from './reducers'
 
-const store = createStore(reducer)
+import { helloSaga } from './sagas'
+
+//创建一个 Saga middleware 和要运行的 Sagas（目前我们只有一个 helloSaga）
+// 将这个 Saga middleware 连接至 Redux store.
+const store = createStore(reducer,applyMiddleware(createSagaMiddleware(helloSaga)))
 
 const action = type => store.dispatch({type})
 
@@ -16,7 +21,9 @@ function render() {
     <Counter
       value={store.getState()}
       onIncrement={() => action('INCREMENT')}
-      onDecrement={() => action('DECREMENT')} />,
+      onDecrement={() => action('DECREMENT')}
+      onIncrementAsync={() => action('INCREMENT_ASYNC')}
+    />,
     document.getElementById('root')
   )
 }
